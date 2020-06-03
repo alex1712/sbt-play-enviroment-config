ba9d40f75eccdbaa1cc3a2d581bed4e6b8341e28
Alejandro Sellero <alejandro.sellero@gmail.com>
2020-06-03 14:23:14 +0200

    Running in the docker container with the config file

--- src/main/scala/com/vs/sbt/PlayEnvironmentDependentConfigPlugin.scala
+++ src/main/scala/com/vs/sbt/PlayEnvironmentDependentConfigPlugin.scala
@@ -1,14 +1,19 @@
 package com.vs.sbt
 
+import com.typesafe.sbt.packager.Keys.executableScriptName
+import com.typesafe.sbt.packager.docker.DockerPlugin
+import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
 import play.sbt.PlayScala
 import sbt._
 import sbt.Keys._
 import sbt.plugins.JvmPlugin
+import com.typesafe.sbt.packager.docker._
+import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport.defaultLinuxInstallLocation
 
 object PlayEnvironmentDependentConfigPlugin extends AutoPlugin {
 
   override def trigger  = allRequirements
-  override def requires = JvmPlugin && PlayScala
+  override def requires = JvmPlugin && PlayScala && DockerPlugin
 
   object autoImport {
     val environmentConfigTask = taskKey[Unit]("Changes the config.resource property file based on the environment")
@@ -21,6 +26,12 @@ object PlayEnvironmentDependentConfigPlugin extends AutoPlugin {
       System.setProperty("config.resource",
                          System.getProperty("config.resource", s"application-${System.getenv("SCALA_ENV")}"))
     },
-    compile in Compile := ((compile in Compile) dependsOn (environmentConfigTask)).value
+    compile in Compile := ((compile in Compile) dependsOn (environmentConfigTask)).value,
+    dockerCommands += Cmd("RUN", "apt update && apt upgrade && apt install dumb-init"),
+    dockerEntrypoint := Seq("/usr/bin/dumb-init", "--"),
+    dockerCmd := Seq(
+      "bash",
+      "-c",
+      s"exec ${(defaultLinuxInstallLocation in Docker).value}/bin/${executableScriptName.value} -Dconfig.resource=application-$${SCALA_ENV}.conf")
   )
 }
